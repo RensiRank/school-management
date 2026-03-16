5973f14bf42709a867269fb85cbd1768fc708500
RensiRank <rensirank11@gmail.com>
2026-03-16 11:05:30 +0530

    stu

--- frontend/src/pages/admin/studentRelated/AddStudent.js
+++ frontend/src/pages/admin/studentRelated/AddStudent.js
@@ -88,7 +88,7 @@ const AddStudent = ({ situation }) => {
         <>
             <div className="register">
                 <form className="registerForm" onSubmit={submitHandler}>
-                    <span className="registerTitle">Add Student for related class</span>
+                    <span className="registerTitle">Add Student for related</span>
                     <label>Name</label>
                     <input className="registerInput" type="text" placeholder="Enter student's name..."
                         value={name}
